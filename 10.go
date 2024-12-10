2289d66a3eec511fbfb9def37d9f525357ce6836
Marat <ban54894@gmail.com>
2024-12-10 20:42:08 +0500

    Initial commit for L1.10

---
+++ 10.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	temperatures := []float64{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
+	groups := make(map[int][]float64)
+
+	for _, temp := range temperatures {
+		groupKey := int(math.Floor(temp/10) * 10) // Группируем значения по шагу в 10 градусов
+		groups[groupKey] = append(groups[groupKey], temp)
+	}
+
+	// Вывод результата
+	for key, temps := range groups {
+		fmt.Printf("%d: %v\n", key, temps)
+	}
+}
